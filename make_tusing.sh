f0a34239d79f42bbd11b30218a15f777bf14962b
Tushar Singal <tsingal@pivotal.io>
2017-06-06 13:50:57 -0700

    Slightly modified keymap; added script to flash.
    - Keymap now supports right ctrl, alt, GUI keys
    - Harder to get into flash mode (caps+enter)
    - Script to flash my keymap (barebones, not very robust at the moment)

---
+++ make_tusing.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+printf "Making the tusing keymap..."
+
+printf "\n\n============ DIFF =============\n"
+git diff
+
+printf "\n\n========== BUILDING ===========\n"
+cd keyboards/zeal60/ && make tusing && cd ../../
+
+
+flash_keymap()
+{
+    printf "\n\n========== FLASHING ===========\n"
+    printf "You have 5 seconds to put your keyboard into programming mode before flashing.\n\n"
+    sleep 5
+    dfu-programmer atmega32u4 erase --force
+    dfu-programmer atmega32u4 flash zeal60_tusing.hex
+    dfu-programmer atmega32u4 reset
+
+    printf "\n\n==========  KEYMAP  ============\n"
+    echo "Attempted to flash keymap:"
+    cat keyboards/zeal60/keymaps/tusing/keymap.c
+}
+
+
+read -p "Flash this keymap? (y/n)" choice
+case "$choice" in
+    y|Y ) flash_keymap;;
+    n|N ) echo "Aborting flash." && exit;;
+    * ) echo "Invalid response." && exit;;
+esac
